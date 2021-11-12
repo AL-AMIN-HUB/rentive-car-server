4b32d90822f01bb9e94926f8a95056e3cec6f153
Md-Alamin-hub <86272640+Md-Alamin-hub@users.noreply.github.com>
2021-11-12 21:52:26 +0600

    product delete

--- index.js
+++ index.js
@@ -107,6 +107,14 @@ async function run() {
       res.json(result);
     });
 
+    //
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.json(result);
+    });
+
     // save users
     app.post("/users", async (req, res) => {
       const user = req.body;
